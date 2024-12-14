8e22c1b927484b020e1243b1b57d8462e9b11e7a
EmiliaKopyta <115356033+EmiliaKopyta@users.noreply.github.com>
2024-12-14 13:08:16 +0100

    Update fruits.js

--- fruits.js
+++ fruits.js
@@ -1,10 +1,14 @@
 const fruits = ['Apple', 'Banana', 'Orange', 'Grape', 'Mango', 'Pineapple', 'Strawberry'];
 const basket = [];
 
-if (Math.random() > 0.5) basket.push('Apple'); //50% szans na jabłko
-console.log('Basket:', basket);
+if (Math.random() > 0.5) { //50% szans na jabłko
+  basket.push('Apple');
+} else { //jeśli nie jabłko, to inny owoc z listy
+  const otherFruits = fruits.filter(fruit => fruit !== 'Apple'); 
+  const randomFruit = otherFruits[Math.floor(Math.random() * otherFruits.length)];
+  basket.push(randomFruit);
+}
 
-// Export basket to use in the test
 var basketString = JSON.stringify(basket);
 const fs = require('fs');
 fs.writeFileSync("basket.json", basketString);
