ec7a4d8e6418f622647b2748264ddf6e5543e5bf
EmiliaKopyta <115356033+EmiliaKopyta@users.noreply.github.com>
2024-12-14 10:27:03 +0100

    Update fruits.js

--- fruits.js
+++ fruits.js
@@ -1,1 +1,9 @@
-fruits
+const fruits = ['Apple', 'Banana', 'Orange', 'Grape', 'Mango', 'Pineapple', 'Strawberry'];
+const basket = [];
+
+if (Math.random() > 0.5) basket.push('Apple'); //50% szans na jabłko
+console.log('Basket:', basket);
+
+// Export basket to use in the test
+const fs = require('fs');
+fs.writeFileSync('basket.json', JSON.stringify(basket, null, 2));
